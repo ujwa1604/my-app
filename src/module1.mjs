718d1a5962a9afc1658cc835a54010fbe6e567ca
Ujwal Vikas shinde <ujwal16042002@gmail.com>
2022-01-07 18:47:32 +0530

    Updates

--- src/module1.mjs
+++
@@ -1,5 +0,0 @@
-import dza, {a,b,c} from './module2.mjs'
-console.log(dza);
-console.log(a);
-console.log(b);
-console.log(c);
--- src/module2.mjs
+++
@@ -1,9 +0,0 @@
-const a="Harry";
-const b="Ujwal";
-const c="Rohan";
-const d="jayu";
-
-export default b;
- export {a};
- export {b};
- export {c};
